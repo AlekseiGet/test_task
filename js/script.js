2fa125308fe2cf294653a80d4a4437aff13bf7f9
AlekseiGet <98254678+AlekseiGet@users.noreply.github.com>
2022-05-09 21:36:15 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,706 +1,78 @@
 
-  /* let hu = 'Я ';
-   let du = 'Учу ';
-   let vot = 'JS';
-   hu = 'Ты ';
-   console.log(hu + du + vot);  
-   console.log(' Я учу JS '); */
 
-/*function testBlock() {
-  let myAge = 45;
-  console.log(myAge);
-
-}
-testBlock();
-console.log(myAge);*/
-
-/*let myAge = 45;
-function testBlock() {
-   let myAge =18;
-   console.log(myAge);
-}
-testBlock();
-console.log(myAge);*/
-
-/*for (let i = 0; i < 3; i++) {
-  console.log(i); 
-}
-for (let i = 0; i < 3; i++) {
-  console.log(i);
-  
-}*/
-/*const userProfile = {
-  name: 'frilanser',
-  age:  45,
-  messege: 'Живи и не работай',
-}
-console.log(userProfile);
-userProfile.age = 99;
-console.log(userProfile);*/
-
-
-/*"use strict"
-age = 45;
-console.log(age);
-var age;*/
-
-/*function testBlock() {
-  let myAge = 34;
-  console.log(myAge);
-}
-testBlock();
-//console.log(myAge);
-
-function testBlock_2() {
-  var myAge = 55;
-  console.log(myAge);
-}
-testBlock_2();
-console.log(myAge);*/
-
-
-/*if (true) {
-  let size = 50; 
-}
-console.log(size);
- if (true) {
-   var size = 33;
- }
- console.log(size);*/
-
-
- /*let userColorEyes ;
- let user;
- let userName;
- userName = "Вася";
- user = userName;
- console.log(user);*/
-
- /*let userName;
- console.log(typeof userName);
- console.log(userName);
- userName = "Алексей";
- console.log(typeof userName);
- userName = 45 ;
- console.log(typeof userName);*/
-
-
-/*let userName;
- console.log(typeof userName);
- console.log(userName);
- if(userName === undefined){
-   console.log("переменная не определена");
- }else {
-   console.log("переменная определена");
- }
- if (typeof userName === 'undefined') {
-   console.log("Пременная не опредена");  
- }else {
-   console.log(" Переменная определена");
- }*/
-
- /* let userName = null;
-  console.log(userName);*/
-
-
- /* let block = document.querySelector('.block');
-  console.log(block);
-  console.log(typeof block);*/
-
- /* let willYouMarryMe = false;
-  if (willYouMarryMe) {
-    console.log(':-)');
-  }else {
-    console.log(':-(');
-  }console.log(typeof willYouMarryMe);*/
-
-  /*let trueOrFalse = 45 < 18;
-  console.log(trueOrFalse);*/
-
-  /*let userAge = 20;
-  let userHeight = 1.79;
-  console.log(userAge); 
-  console.log(typeof userAge);
-  console.log(userHeight);
-  console.log(typeof userHeight);*/
-
-  /*let getInfinity = 45/0;
-  console.log(getInfinity);*/
-
- /* let userAge =30 - 12;
-  console.log(userAge);
-  let userHeight = -24;
-  console.log(userHeight);*/
-
- /* let x;
-  x = 5 - 1;
-  console.log(`результат сложения: ${x}`);
-  x = 9 - 3;
-  console.log(`результат вычитания: ${x}`);
-  x = 16 * 13;
-  console.log(`результата умножения:${x}`);
-  x = 12 / 5;
-  console.log(`результат деления:${x}`);
-  x = 12 % 5;
-  console.log(`результат взятия остатка от деления: ${x}`);
-  x = 5 ** 12;
-  console.log(`результат возведения в степень: ${x}`);*/
-
-  /*let resultOne = "Гетманов" + " " + "Алексей" + " " + "Владимирович";
-  console.log(resultOne);
-  let resultTwo = "Мой рост" + " " + 179 + " " + "см."
-  console.log(resultTwo);
-  console.log(typeof resultTwo);
-  let resultThree = '2' + 2;
-  console.log(resultThree);
-  console.log(typeof resultThree);
-  let resultFour = 100 - 55 + " " + "Мой возвраст";
-  console.log(resultFour);
-  console.log(typeof resultFour);
-  let resultFive = "Мне" + " " + 100 - 55 +  " " + "лет";
-  console.log(resultFive);
-  console.log(typeof resultFive);*/
-
-  /*let resultOne = "50" - 5;
-  console.log(resultOne);
-  console.log(typeof resultOne);
-
-  let resultTwo = '50' * 3;
-  console.log(resultTwo);
-  console.log(typeof resultTwo); 
-
-  let resultThree = "trhee" / 3;
-  console.log(resultThree);
-  console.log(typeof resultThree);*/
-
-  /*let resultOne = '33';
-  console.log(resultOne);
-  console.log(typeof resultOne);
-  resultOne  = +resultOne ;
-  console.log(resultOne);
-  console.log(typeof resultOne);
-
-  let resultTwo = "55";
-  let resultThree = "45";
-  console.log(resultTwo + resultThree);
-  console.log(resultTwo - resultThree);
-  console.log(Number (resultTwo) + Number (resultThree));
-  console.log(+resultTwo + +resultThree)*/
-
- /* let a = 1 + 2;
-  let b = 3;
-  let result = 9 -(a = b + 3);
-  console.log(result); */
-
- /* let user = 5;
-  console.log(user);
-  user = user + 3;
-  console.log(user);
-  user -= 3;
-  console.log(user);
-  user++;
-  console.log(user);
-  ++user;
-  console.log(user);
-  console.log(++user);
-  console.log(user++);
-  console.log(user);*/
-
- /* console.log(1 > 2);       //false
-  console.log("44" === 44);   //false
-  console.log('44' !== 44);   //true
-  console.log("44" == 44);    //true
-  console.log('44' != 44);    //false  */
-
- /* console.log(1 || 0); //1
-  console.log(true || "Alexsei"); //true
-  console.log(null || 56); // 56
-  console.log(null || "Alexsei"); //Alexsei
-  console.log(undefined || '' || null || 0); //0*/
-
-
-  /*let userName;
-  let userNickName = "Aleksei";
-  let user = userName || userNickName || "no name";
-  console.log(user);*/
-
- /* let user = 10;
-  let admin = 15;
-  user > admin || user++;
-  console.log(user);*/
-
-  /*console.log('aleksei' && 0 && 2);//0
-  console.log(1 && 2 && null && 3);//null*/
-
-  /*let users = 10;
-  (users > 1) && console.log(`пользователей ${users}`);*/
-
- /* let a = null;
-  console.log(!null);   
-  console.log(Boolean (a));*/
-
-//console.log(!true && 65 || 33 && !1);//false
-
-
-/*let name ;
-console.log(name ?? 'нет имени' ?? '');//нет имени*/
-
-/*let messege = 'Привет';
-let messegeName ;
- if (5>1) {
-   messegeName = ', Алексей!';
- } else {
-   messegeName = ', Наташа!';
- }
- messege = messege + messegeName;
- console.log(messege);*/
-
- /*let messege = 'hello';
- let messegeName = (5 < 1) ? ', Aleksei!' : ', Natahsa!';
- messege += messegeName;
- console.log(messege);*/
-
-/* let messege = 'Привет';
- let messegeName;
- if (5 > 50) {
-   messegeName = "Aleksei";
- } else if (5 > 30) {
-   messegeName = ", Natasha!";
- } else if (5 > 20) {
-   messegeName = ", Valera!";
- } else if (5 > 10) {
-   messegeName = ", Masha!";
- }else {
-   messegeName = ", Babushka";
- }
- messege += messegeName;
- console.log(messege);*/
-
-/* let messege = 'Привет';
- let messegeName = 5 > 50 ? ', Aleksei': 5 > 30 ? ', Natasha': 5 > 20 ? ', Valera': 5 > 10 ? ', Masha': ', Babushka';
- messege += messegeName;
- console.log(messege);*/
-
- /*let messege = (92 > '11') ? 'true' : 'false';
- console.log(messege);*/
-
- // Циклы
-
-
- /*let num = 6;
- while (num < 10) {
-   console.log(num);
-   num++;
- }*/
-
- /*let num = 0;
- do {
-   console.log(num);
-   num++;
- } while (num < 0);*/
-
-/* for (let num = 0; num < 5; num++) {
-   console.log(num);  
- }*/
-
- /*let num;
- for ( num = 0; num < 5; num++) {
-   console.log(num );
-   
- }
-console.log(`Вывод вне цикла: ${num}`);*/
-
-/*let num = 0;
-for(; num < 5; ){
-	console.log(num);
-	num++;
-}
-console.log(`Вне цикла ${num}`);*/
-
-/*let num = 0;
-for (; num < 9;){
-	num ++;
-	console.log(num); 
+function loadData(url) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            let tokenMass = JSON.parse(xhttp.responseText);
+           
+            for (let i = 0; i < tokenMass.length; i++) {
+                
+               /* 
+               const newElement = document.createElement('div');
+                newElement.classList.add('noda');
+                newElement.innerHTML = tokenMass;
+                const subListNoAll = document.querySelector(".slider__token__conteiner");
+                subListNoAll.append(newElement);
+              */
     
-	if (num == 3)break;//Работа окончена!
+                let db_id = tokenMass[i].db_id;
+                let name_token = tokenMass[i].name;
+                let description_token = tokenMass[i].description;
+                const newElement = document.createElement('div');
+                newElement.insertAdjacentHTML('afterbegin', ` db_id: ${db_id}, name: ${name_token}, description: ${description_token}`);
+                 newElement.classList.add('noda');
+                const subListNoAll = document.querySelector(".slider__token__conteiner");
+                subListNoAll.append(newElement);
+               
+            }
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
 }
-console.log("Работа окончена!");*/
-
-/*let num = 0;
-for (; num < 5; num++){
-	if (num == 2)continue;
-  console.log(num);
-}*/
-
-/*let num = 0;
-for (; num < 5; ){
-	num++;
-	if (num == 2)continue;
-    console.log(num);
-}*/
+loadData("https://api.expandingspace.io/beta/Tokens?username=")
 
 
-/*firstFor: for (let num = 0; num < 7; num++){
-	for (let size = 10; size < 15; size++){
-		if (size == 14 ){
-			break firstFor;
-		}
-		
-		console.log(size);
-	}
-	console.log(num);
-}*/
-
-/*firstFor: for (let num = 0; num < 2; num++){
-	for (let size = 10; size < 13; size++){
-		if (size == 12 ){
-			continue firstFor;// переход на внешний цикл 
-     // break firstFor; или прерываем
-		}
-		console.log(size);
-	}
-	console.log(num);
-}*/
-
-
-//задача 1
-
-/*let num = 0;
-while ( num < 5){
-	num++;
-	console.log(num);
-}*/
-
-/*let num = 0;
-do {
-	num++;
-	console.log(num);
-}while(num < 5);*/
-
-/*for (let num = 1;num < 6;num++){
-	console.log(num);
-}
-*/
-
-
-/*let num = 0;
-for(;;){
-	num++;
-	if(num >= 6)break;
-	console.log(num);
-}*/
-
-/*let num = 0;
-while( num >= 0){
-      num++;
-      if(num == 6)break;
-      console.log(num);
-      }*/
-
-/*let num = 8;
-while (num){
-    console.log(num);
-    num--;
-}*/
-
-
-/*for (let num = 0; num < 3; num++) {
-  console.log(`Число: ${num}`);
-  
-}*/
-
-
-/*let num = 0;
-while (num < 3) {
-  console.log(`Число: ${num}`);
-  num++;
-}*/
-
-/*firstFor: for (let num = 0; num < 2; num++) {
-  for (let size = 10; size < 13; size++) {
-    if (size == 11) {
-      break firstFor;
-    }
-    console.log(size);   
-  }
-  console.log(num);
-}*/
-
-
-
-// Ф У Н К Ц И И
-
-
-
-/*let num = 13;
-let str = 'abc';
-function showMessegeNum() {
-  console.log(num);
-}
-function showMassegeStr() {
-  console.log(str);
-}
-showMassegeStr();
-showMessegeNum();
-showMessegeNum();
-showMassegeStr();*/
-
-
-/*let messege ="Скриншот";
-function showMessege() {
-  messege = 'Сообщение';
-  console.log(messege);
-};
-function showMessegeAlt() {
-  messege = 'Вызов';
-}
-console.log(messege);
-showMessege();
-console.log(messege);
-showMessegeAlt();
-console.log(messege);
-showMessege();
-console.log(messege);
-console.log(messege);
-showMessegeAlt();
-console.log(messege);
-
-console.log(messege);*/
-
-/*let globalVar = 'Глобальная переменная';
+let noda = document.getElementsByClassName('noda');
+const subListNoAll = document.querySelector(".slider__token__conteiner");
  
-function getSumm() {
-   let numOne, numTwo, numSumm;
-   
-   function getNumOne() {
-     numOne = 1;
-     console.log(globalVar + " " + numOne );
-   }
-   function getNumTwo() {
-     numTwo = 2;
-   }
-
-   console.log(numOne);
-  getNumOne();
-  
-  console.log(numTwo);
-  getNumTwo();
-  numSumm = numOne + numTwo; 
-  console.log(numOne);
-  console.log(numTwo);
-   console.log(numSumm);
-}
-getSumm();
-console.log(globalVar );*/
-
-/*let messege ='Сообщение №1';
-function showMessege() {
-  let messege = 'Сообщение №2';
-  console.log(messege);
-}
-showMessege();
-console.log(messege);
-*/
-
-/*function calcSumm(numOne, numTwo, numSumm) {
-  numSumm = numOne + numTwo;
-  console.log(`Сумма: ${numSumm}`);
-}
-calcSumm(2, 3,);*/
-
-
-/*function showMoreMessege() {
-  console.log("Больше чем 3");
-}
-function showLeesMessege() {
-  console.log("Меньше чем 3");
-}
-function calcSumm(numOne, numTwo, more, less) {
-  let numSumm = numOne + numTwo;
-  if (numSumm > 3) {
-    more() ;
-  } else {
-    less();
-  }
-}
-
-calcSumm(1, 5, showMoreMessege, showLeesMessege);*/
-
-
-
-/*function calcSumm(numOne, numtwo) {
- let numSumm = numOne + numtwo;
-  return numSumm;
-}
-  let functResult = calcSumm(1, 2);
-  console.log(`Сумма: ${functResult}`);*/
-
-
-// Р Е К У Р С И Я
- /* function getSumm(numOne, numTwo) {
-     let numSumm = calcSumm(numOne, numTwo);
-     console.log(numSumm);
-  }
-  function calcSumm(numOne, numTwo) {
-    return numOne - numTwo;
-  }
-getSumm(6, 7);*/
-
-
-/*function calcSumm(numOne, numTwo) {
-  let result = 1;
-  for (let i = 0; i < numTwo; i++) {
-    result *= numOne;
-  }
- return result;
-}
-console.log(calcSumm(2, 3));*/
-
-// То же самое:
-
-/*function calcSumm(numOne, numTwo) {
-  if (numTwo === 1) {
-    return numOne;
-  } else {
-    return numOne * calcSumm(numOne, numTwo-1);
-  }
-}
-console.log(calcSumm(3, 2 ));*/
-
-// ФУНКЦИОНАЛЬНОЕ ВЫРАЖЕНИЕ
-
-/*let showMessege = function (){
-  console.log('Привет !');
-}
-showMessege(); */
-
-/*function calcSumm(numOne,numeTwo) {
-  let numSumm = numOne + numeTwo;
-  console.log(`Сумма:${numSumm}`);
-}
-let getSumm = calcSumm;
-calcSumm(3, 6); //обратится к функции напрямую
-getSumm(7, 6);*/ //обратится к функции через переменную
-
-
-/*'use strict'
-let getSumm ;
-if (3 > 12) {
-  getSumm = function (numOne, numTwo) {
-    let numSumm = numOne - numTwo;
-    console.log(`Больше на: ${numSumm}`);
-  }
-} else {
-  getSumm = function (numOne, numTwo){
-  console.log('Нифига не больше!!');
-  }
-}
-getSumm(58, 77);*/
-
-/*'use strict'
-let  getSumm;
-if (2 > 1) {
- getSumm = function() {
-    let summ = 1 + 2;
-    console.log(summ);
-  };
-}
-getSumm();*/
-
-
-//СТРЕЛОЧНАЯ ФУНЦИЯ
- /* let roboMessege = (text, messege) => text + ' ещё ' + messege + '!!';
-  console.log(roboMessege('Хочу', 'денег'));*/
-
-/*let roboMessege = (text, name) => {
-  let messege = text + " ещё " + name;
-  return messege;
-};
-  console.log(roboMessege('ДАВАЙ', "денег"));*/
-
-  // setTimeout
-
-  /*function showMessege(text, name) {
-    console.log(`${text}, ${name}!!`);
-  }
-  showMessege("Привет", "кожаный мешок")
-  setTimeout(showMessege, 500,'привет',"Повелитель")*/
-
-  //РЕКУРСИВНЫЙ SETTIMEOUT
- /* function showMessege(text, name) {
-    console.log(`${text}, ${name} !`);
-    setTimeout(showMessege, 500, text, name);
-  }
-  setTimeout(showMessege, 500, 'Привет', "Мир !")*/
-
-  //РЕКУРСИВНЫЙ SETTIMEOUT и if
-/*function showNumber(num) {
-  console.log(num);
-  if(num < 9){
-  setTimeout(showNumber, 1000, ++num);
-  }
-}
-setTimeout(showNumber, 1000, 1);*/
-
- // ПЛАНИРОВАНИЕ SETTIMEOUT SETINTERVAL
-  /*function showNumber(num) {
-    console.log(num);
-    let timeId = setTimeout(showNumber, 1000, ++num);
-    if (num === 7) {
-      clearTimeout(timeId);
-    }
-  }
-  setTimeout(showNumber, 1000, 1);*/
-
-
-    //СОЕДИНЕНИЕ СТРОК
-   /* function creatMessege(text, name) {
-      return (`${text}, ${name}!`);
-    }
-    function showMessege(messege) {
-      console.log(messege);
-    }
-    //обьеденяющая функция
-    function initMassege(text, name) {
-      showMessege(creatMessege(text, name));
-      
-    }
-    initMassege("Hello", "world");*/
-
-    //ДОМАШНЯЯ ЗАДАЧА
-
-    // 1
-    /*function showName() {
-      console.log("Вася");
-    }
-    //showName(); первым будет Вася
-    setTimeout(showName, 0);// а так первым будет Коля
-    console.log("Коля");*/
-
-    // 2
-   /* showMessege();
-    function showMessege() {
-      console.log('Messege');
-      // да
-    }*/
-
-    // 3
-   /* showMessege();
-    let showMessege =  function () {
-      console.log('Messege');
-    }  //нет */
- 
-
-    // 4
-   /* "use strict"
-   let showMessege;
-   if (2 > 1) {
-      showMessege = function () {
-       console.log("Сообщение");
-     }
-   }
-     showMessege();*/
-
-     //ОБЬЕКТЫ
-
+const mainForm = document.forms.main.token_sort;
+mainForm[0].addEventListener('change', function (e) {
+     subListNoAll.style.color = 'white';
      
+     console.log( noda[0]);
+
+})
+
+mainForm[1].addEventListener('change', function (e) {
+    subListNoAll.style.color = 'red';
+    console.log(Object.entries(noda[1]));
+})
+
+mainForm[2].addEventListener('change', function (e) {
+    subListNoAll.style.color = 'blue';
+    console.log(noda[2]);
+})
+
+mainForm[3].addEventListener('change', function (e) {
+    subListNoAll.style.color = 'green';
+   console.log(noda[3]);
+ 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
